1dc5a4485ce7af1d128bde18d85f7d8d37a08ba1
Aren S <adsalmela@gmail.com>
2021-02-24 18:42:58 -0500

    About Me component template.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header"
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import AboutMe from "./components/AboutMe"
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AboutMe/>
     </div>
   );
 }
